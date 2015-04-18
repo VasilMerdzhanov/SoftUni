5ba4d67c1a5902c9b6d73d89d3f9f0d9c2335a8f
Katya Marincheva <katya.marincheva@outlook.com>
2015-04-19 01:17:03 +0300

    Updating Homeworks repository

---
+++ SoftUni-Homeworks/07. Loops-Homework/01. Numbers-From-1-To-N/NumbersFrom1ToN.cs
@@ -0,0 +1,30 @@
+﻿// Problem 1. Numbers from 1 to N
+
+/* Write a program that enters from the console a positive integer n 
+ * and prints all the numbers from 1 to n, on a single line, separated by a space.
+
+Examples:
+n 	output
+3 	1 2 3
+5 	1 2 3 4 5
+ */
+
+using System;
+
+class NumbersFrom1ToN
+{
+    static void Main()
+    {
+        // input
+        Console.Write("Please, enter a value for the int n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // logic
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("{0} ", (i + 1));
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ SoftUni-Homeworks/07. Loops-Homework/10. Odd-And-Even-Product/OddAndEvenProduct.cs
@@ -0,0 +1,60 @@
+﻿// Problem 10. Odd and Even Product
+
+/* You are given n integers (given in a single line, separated by a space).
+   Write a program that checks whether the product of the odd elements is equal to the product of the even elements.
+   Elements are counted from 1 to n, so the first element is odd, the second is even, etc.
+
+Examples:
+numbers 	result
+2 1 1 6 3 	yes
+product = 6 	
+	
+3 10 4 6 5 1 	yes
+product = 60 	
+	
+4 3 2 5 2 	no
+odd_product = 16 	
+even_product = 15 	
+    */
+
+using System;
+using System.Linq;
+
+class OddAndEvenProduct
+{
+    static void Main()
+    {
+        // declarations
+        string input;
+        int productOdd = 1;
+        int productEven = 1;
+
+        // input
+        Console.Write("Please, enter 5 numbers in a single line: ");
+        int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray(); // second, int array, ready to store the parsed number strings
+
+        // logic
+        for (int i = 0; i < nums.Length; i++)
+        {           
+            // the odd elements (1, 3, etc.) are at index 0, 2, etc.; so if the index is even, the element will be odd
+            if (i % 2 == 0)
+            {
+                productOdd *= nums[i]; // product of odd elements
+            }
+
+            else
+            {
+                productEven *= nums[i]; // product of even elements
+            }
+        }
+
+        // printing and evaluation
+        Console.WriteLine("\nNumbers: {0}", String.Join(", ", nums));
+        Console.WriteLine("Product of the odd elements: {0}", productOdd);
+        Console.WriteLine("Product of the even elements: {0}", productEven);
+
+        // display Yes for equal products, No if the products of odd and even elements are not equal
+        Console.WriteLine("The product of the odd elements is equal to the product of the even elements: {0}\n", (productOdd == productEven)? "yes" : "no");
+    }
+}
+
