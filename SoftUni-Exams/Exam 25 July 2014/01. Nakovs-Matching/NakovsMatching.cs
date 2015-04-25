fc72bcfb0c92abc3e00599efe4c4411d3be7e9af
Katya Marincheva <katya.marincheva@outlook.com>
2015-04-25 23:06:00 +0300

    Exam 25 July 2014

---
+++ SoftUni-Exams/Exam 25 July 2014/01. Nakovs-Matching/NakovsMatching.cs
@@ -0,0 +1,90 @@
+﻿/* Problem 4 – Nakovs Matching
+We are given two words a and b. Each word can be split in several ways into left and right side:
+•	a = (aLeft|aRight)
+•	b = (bLeft|bRight)
+The weight w(s) of given character sequence s is calculated as sum of the ASCII codes of its characters. 
+ * The Nakov's distance between two split words (aLeft|aRight) and (bLeft|bRight) is defined as:
+•	nakovs = w(aLeft) * w(bRight) - w(aRight) * w(bLeft), as absolute value
+We assume that two word splits have good matching if their Nakov's difference is not greater than given limit number d. 
+ * Your task is to write a program that prints all good matchings between given two words a and b and given limit number d.
+Example: a = "hello", b = "csharp", d = 20000. The word a can be split in 4 ways: "h|ello", "he|llo","hel|lo" and "hell|o". 
+ * The word b can be split in 5 ways: "c|sharp", "cs|harp", "csh|arp", "csha|rp" and "cshar|p". 
+ * There are 20 possible matchings between the words a and b, but only 3 of them are good matchings (nakovs ≤ d):
+(h|ello) matches (c|sharp) by 13996 nakovs	w(h) = 104, w(ello) = 428, w(c) = 99, w(sharp) = 542
+nakovs = abs(104 * 542 - 428 * 99) = 13996 ≤ 20000
+(he|llo) matches (cs|harp) by 17557 nakovs	w(he) = 205, w(llo) = 327, w(cs) = 214, w(harp) = 427
+nakovs = abs(205 * 427 - 327 * 214) = 17557 ≤ 20000
+(hell|o) matches (cshar|p) by 11567 nakovs	w(hell) = 421, w(o) = 111, w(cshar) = 529, w(p) = 112
+nakovs = abs(421 * 112 - 529 * 111) = 11567 ≤ 20000
+Your task is to write a program that enters a, b and d and prints on the console all good matchings between a and b.
+Input
+The input data should be read from the console. It consists of 3 lines:
+•	The first line hold the first string.
+•	The second line holds the second string.
+•	The third line holds the limit number d.
+The input data will always be valid and in the format described. There is no need to check it explicitly.
+Output
+Print on the console all good matchings between these two words in format "(aLeft|aRight) matches (bLeft|bRight) by XXX nakovs",
+ * each at separate line. The order of the output lines is not important. Print "No" if no good matchings are found.
+Constraints
+•	The input words a and b consist of small Latin letters only. The length of the words is in the range [2…20].
+•	The number d is integer in the range [0…5 000 000].
+ */
+
+using System;
+using System.Linq;
+
+class NakovsMatching
+{
+    static void Main()
+    {
+        string first = Console.ReadLine();
+        first = first.ToLower();
+        string second = Console.ReadLine();
+        second = second.ToLower();
+        int D = int.Parse(Console.ReadLine());
+        int count = 0;
+
+        for (int i = 0; i < first.Length -1; i++)
+        {
+            string a = first.Substring(0, i + 1);
+            string b = first.Substring(i + 1);
+            int wA = CalcWeight(a);
+            int wB = CalcWeight(b);
+
+            for (int j = 0; j < second.Length - 1; j++)
+            {
+                string c = second.Substring(0, j + 1);
+                string d = second.Substring(j + 1);
+                int wC = CalcWeight(c);
+                int wD = CalcWeight(d);
+
+                int match = Math.Abs(wA * wD - wB * wC);
+                if ( match <= D)
+                {
+                    Console.WriteLine("({0}|{1}) matches ({2}|{3}) by {4} nakovs", a, b, c, d, match);
+                    count++;
+                }
+            }
+        }
+
+        if (count == 0)
+        {
+            Console.WriteLine("No");
+        }
+    }
+
+    private static int CalcWeight(string s)
+    {
+        char[] arr = s.ToCharArray();
+
+        int weight = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            weight += arr[i];
+        }
+
+        return weight;
+    }
+}
+
---
+++ SoftUni-Exams/Exam 25 July 2014/05. Change-Even-Bits/ChangeEvenBits.cs
@@ -0,0 +1,68 @@
+﻿/* Problem 5 – Change Even Bits
+You are given a number N and you have to read from the console exactly N + 1 integers and additionally an integer 
+ * L – the number to be processed. Your task is to count the bits in each of the N input integers (let this be the number len), 
+ * then change bit to "1" len even positions (0, 2, 4, …) of the input number L. 
+ * Print on the console the obtained number and the total number of bits that have actually been changed. 
+ * To be counted as changed they should have been “0” at first and then changed to “1”. 
+ * The comments in the example below will help you understand better what you should do. Note that "0" consists of 1 bit (len=1).
+Input
+The input data should be read from the console.
+•	The first line holds an integer number N – the count of numbers to be read.
+•	The next N lines hold the N input integers.
+•	The last line holds an integer number L – the number to be processed.
+The input data will always be valid and in the format described. There is no need to check it.
+Output
+The output should be printed on the console. It should consist of exactly 2 lines:
+•	On the first line print the number L after setting its even bits.
+•	On the second line print the number of bits that have actually changed.
+Constraints
+•	N will be an integer between 0 and 10.
+•	The N input integers in the input will be between 0 and 65535.
+•	L will be an integer number between 0 and 18 446 744 073 709 551 615.
+ */
+
+using System;
+
+public class ChangeEvenBits
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+        ulong lastInput = ulong.Parse(Console.ReadLine());
+        const ulong one = 1;
+        int changedBits = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int num = nums[i];
+            int len = 0;
+            do
+            {
+                num = num / 2;
+                len++;
+            } while (num > 0);
+
+            int bitPosition = 0;
+            while (len > 0)
+            {
+                ulong mask = one << bitPosition;
+                if ((lastInput & mask) == 0)
+                {
+                    changedBits++;
+                }
+                lastInput |= mask;
+                bitPosition += 2;
+                len--;
+            }
+        }
+
+        Console.WriteLine(lastInput);
+        Console.WriteLine(changedBits);
+    }
+}
+
