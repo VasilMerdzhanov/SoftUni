8e640eb13227a2d086ca3429c5e9fc3d926d6af7
Katya Marincheva <katya.marincheva@outlook.com>
2015-05-17 00:21:42 +0300

    05. Regular Expressions Homework

---
+++ 05. Regular-Expressions-Homework/02. Replace-AHref-Tag-FromFile/ReplaceAHrefTagFromFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class ReplaceAHrefTagFromFile
+{
+    static void Main()
+    {
+        // read file HTML-AHref.txt saved in same folder as the project .cs file
+        string html = File.ReadAllText(@"..\..\HTML-AHref.txt");
+
+        string pattern = @"<a(.*href=(.|\n)*?(?=>))>((.|\n)*?(?=<))<\/a>";
+
+        // create new file
+        using (StreamWriter URL = new StreamWriter(@"..\..\HTML-URL.txt"))
+        {
+            // write on the new file, the URL-replaced string
+            URL.WriteLine(Regex.Replace(html, pattern, @"[URL$1$2]$3[/URL]"));
+        }
+
+        // print output file to the console
+        string fileContents = File.ReadAllText(@"..\..\HTML-URL.txt");
+        Console.WriteLine(fileContents);
+    }
+}
+
