8f3b148ddb1976178bf11197cc4e7634803b5ae3
Katya Marincheva <katya.marincheva@outlook.com>
2015-05-17 11:48:21 +0300

    05. Regular Expressions Homework

---
+++ 05. Regular-Expressions-Homework/04. Sentence-Extractor/SentenceExtractor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class SentenceExtractor
+{
+    static void Main()
+    {
+        // input 
+        string word = Console.ReadLine();
+        string text = Console.ReadLine();
+
+        // logic - check if substring is a sentence, and if it contains the word
+        MatchCollection matches = IsSentence(text);
+
+        foreach (Match sentence in matches)
+        {
+            string x = sentence.ToString();
+            if (IsWordInSentence(x, word))
+            {
+                // printing
+                Console.WriteLine(x.Trim());
+            }
+        }
+    }
+
+    private static bool IsWordInSentence(string sentence, string word)
+    {
+        return Regex.Matches(sentence, string.Format(@"\b{0}\b", word), RegexOptions.IgnoreCase).Count != 0;
+    }
+
+    private static MatchCollection IsSentence(string text)
+    {
+        string pattern = @"([^.!?]+(?=[.!?])[.!?])";
+        Regex rgx = new Regex(pattern);
+        MatchCollection matches = rgx.Matches(text);
+        return matches;
+    }
+}
+
